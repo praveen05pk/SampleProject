be7e43a67395c034a9d18b2ea5df7db06fd94718
praveen05pk <praveen19sri@gmail.com>
2021-01-21 11:56:25 +0530

    Update

---
+++ src/org/com/Sample.java
@@ -0,0 +1,21 @@
+package org.com;
+
+public class Sample {
+	
+	private void add() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void sub() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void to() {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
